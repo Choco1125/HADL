6778d2107c1491a7f50d77f47469cc184f3ca351
choco1125 <sebascruz981125@gmail.com>
2020-09-06 16:08:44 -0500

    Correción de ortografía en catálogo

--- views/admin/servicios/catalogo.php
+++ views/admin/servicios/catalogo.php
@@ -5,7 +5,7 @@
 
 <div class="container">
     <div class="row justify-content-center mt-2">
-        <h1>Catalogo de servicios</h1>
+        <h1>Catálogo de servicios</h1>
     </div>
     <div class="row mt-1 mb-2 justify-content-end">
         <button class="btn btn-outline-primary btn-sm" data-toggle="modal" data-target="#crear">Crear servicio</a>
